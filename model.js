cbb959c4d61c745b914ebddf704f255112366c92
sakiladissanayake <sakilad@gmail.com>
2018-10-15 09:04:15 +0530

    update config in model.js

---
+++ model.js
@@ -0,0 +1,17 @@
+//config
+var config = {
+	clients: [{
+		clientId: 'saki',
+		clientSecret: 'saki123'
+	}],
+	confidentialClients: [{
+		clientId: 'sakila',
+		clientSecret: 'sakila123'
+	}],
+	tokens: [],
+	users: [{
+		id: '001',
+		username: 'user',
+		password: 'user'
+	}]
+};

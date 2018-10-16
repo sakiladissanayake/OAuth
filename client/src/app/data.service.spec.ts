9af3696224a6618f387beac2644b514001865101
sakiladissanayake <sakilad@gmail.com>
2018-10-16 20:13:02 +0530

    data.service.ts updated

--- client/src/app/data.service.spec.ts
+++ client/src/app/data.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 
-describe('GitHubServiceService', () => {
+describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [DataService]
